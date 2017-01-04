d4e591c422c586900f02510e3e2f9531bde8e76e
jsonzou <keko-boy@163.com>
2017-01-04 21:58:19 +0800

    初始化

---
+++ src/main/java/com/github/jsonzou/App.java
@@ -0,0 +1,13 @@
+package com.github.jsonzou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jmockdata!" );
+    }
+}
