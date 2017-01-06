858c3c0f4240f45fba465fa6b3575f8a234ef2b1
v_zoupengfei <v_zoupengfei@baidu.com>
2017-01-06 13:35:10 +0800

    注释去掉

--- src/test/java/com/github/jsonzou/jmockdata/test/wrapperbean/MockDemoCustomStringBufferWrapper.java
+++ src/test/java/com/github/jsonzou/jmockdata/test/wrapperbean/MockDemoCustomStringBufferWrapper.java
@@ -1,6 +1,3 @@
-/*
- * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
- */
 package com.github.jsonzou.jmockdata.test.wrapperbean;
 
 import com.github.jsonzou.jmockdata.mockdata.JmockDataWrapper;
